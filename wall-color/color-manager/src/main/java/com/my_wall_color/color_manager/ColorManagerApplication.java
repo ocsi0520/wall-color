c4dcb506427710001536f013b20096c45af2c004
Faragó Tamás <ocsi0520@gmail.com>
2025-10-18 09:25:04 +0200

    generate basic spring w/ https://start.spring.io/

---
+++ wall-color/color-manager/src/main/java/com/my_wall_color/color_manager/ColorManagerApplication.java
@@ -0,0 +1,13 @@
+package com.my_wall_color.color_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColorManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColorManagerApplication.class, args);
+	}
+
+}
---
+++ wall-color/color-manager/src/test/java/com/my_wall_color/color_manager/ColorManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my_wall_color.color_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColorManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
