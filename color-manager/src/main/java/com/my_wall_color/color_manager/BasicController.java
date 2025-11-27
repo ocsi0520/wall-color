a96d068659b0e65851cc98d2465ca8b52f98f26b
Faragó Tamás <ocsi0520@gmail.com>
2025-11-27 15:01:09 +0100

    change dummy BasicController

--- color-manager/src/main/java/com/my_wall_color/color_manager/BasicController.java
+++ color-manager/src/main/java/com/my_wall_color/color_manager/BasicController.java
@@ -1,13 +1,17 @@
 package com.my_wall_color.color_manager;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api")
 public class BasicController {
 
-    @GetMapping("/api/hello-world")
-    public String index() {
+    @GetMapping("/hello-world/{whatever}")
+    public String index(@PathVariable(value = "whatever", required = false) String whatever) {
         return "<h1>Hello</h1><h2>world!</h2>";
+    }
+
+    @GetMapping("/asd")
+    public String bla() {
+        return "asd";
     }
 }
