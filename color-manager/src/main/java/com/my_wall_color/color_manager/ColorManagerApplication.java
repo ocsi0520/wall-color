1fe17fd72b8229d27b2bb0049d00b27df189d006
Faragó Tamás <ocsi0520@gmail.com>
2025-12-08 21:56:33 +0100

    add clock bean

--- color-manager/src/main/java/com/my_wall_color/color_manager/ColorManagerApplication.java
+++ color-manager/src/main/java/com/my_wall_color/color_manager/ColorManagerApplication.java
@@ -2,6 +2,9 @@ package com.my_wall_color.color_manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.Clock;
 
 @SpringBootApplication
 public class ColorManagerApplication {
@@ -10,4 +13,10 @@ public class ColorManagerApplication {
 		SpringApplication.run(ColorManagerApplication.class, args);
 	}
 
+    // TODO: log
+
+    @Bean
+    public Clock clock() {
+        return Clock.systemUTC();
+    }
 }
