c0c32ba2b99543366632b30bee996432a77b68a7
Faragó Tamás <ocsi0520@gmail.com>
2025-12-12 15:10:09 +0100

    remove unnecessary endpoints in DummyController

--- color-manager/src/test/java/com/my_wall_color/color_manager/DummyController.java
+++ color-manager/src/test/java/com/my_wall_color/color_manager/DummyController.java
@@ -1,23 +1,13 @@
 package com.my_wall_color.color_manager;
+
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class DummyController {
 
-    @GetMapping("/hello-world/{whatever}")
-    public String index(@PathVariable(value = "whatever", required = false) String whatever) {
-        return "<h1>Hello</h1><h2>world!</h2>";
-    }
-
     @GetMapping("/asd")
     public String bla() {
         return "asd";
-    }
-
-    @PostMapping("/public")
-    public String handlePublic() {
-        System.out.println("Hello World");
-        return "Hello World";
     }
 }
