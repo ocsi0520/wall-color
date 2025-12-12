f874154efcf9d583ac023ce0942691eae64ef9b2
Faragó Tamás <ocsi0520@gmail.com>
2025-12-12 14:51:23 +0100

    use AuthHeaderRetriever for FrontendFilterTest

--- color-manager/src/test/java/com/my_wall_color/color_manager/FrontendFilterTest.java
+++ color-manager/src/test/java/com/my_wall_color/color_manager/FrontendFilterTest.java
@@ -1,53 +1,30 @@
 package com.my_wall_color.color_manager;
 
+import com.my_wall_color.test_utils.PostgresContainerTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.http.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-// TODO: maybe we don't need a separate NoContextTestApp
-//  these can be tested w/ production SecurityFilterChain
-@SpringBootTest(
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-        classes = com.my_wall_color.test_utils.NoContextTestApp.class,
-        properties = {
-                "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"
-        }
-)
-@Import(FrontendFilterTest.TestConfig.class)
-class FrontendFilterTest {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class FrontendFilterTest extends PostgresContainerTest implements AuthHeaderRetriever {
     @Autowired
     TestRestTemplate restTemplate;
 
-    @TestConfiguration
-    @Import({DummyController.class, FrontendFilter.class})
-    static class TestConfig {
-        @Bean
-        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-            return http.csrf(CsrfConfigurer::disable)
-                    .authorizeHttpRequests(authz -> authz.anyRequest().permitAll()).build();
-        }
-    }
-
     @Test
     void shouldNotReturnHTMLForApi() {
-        var asdResponse = restTemplate.getForEntity("/api/asd", String.class);
-        assertThat(asdResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(asdResponse.getHeaders().getContentType().includes(MediaType.TEXT_HTML)).isFalse();
+        var entity = new HttpEntity<>(getAuthIncludedHeadersWith(restTemplate));
+        ResponseEntity<String> response = restTemplate.exchange("/api/asd", HttpMethod.GET, entity, String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getHeaders().getContentType().includes(MediaType.TEXT_HTML)).isFalse();
+        assertThat(response.getHeaders().getContentType().includes(MediaType.TEXT_PLAIN)).isTrue();
     }
 
     @Test
-    void shouldNotReturnHTMLForStatic() {
+    void shouldNotReturnHTMLForStaticIcon() {
         var iconResponse = restTemplate.getForEntity("/favicon.ico", String.class);
         assertThat(iconResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(iconResponse.getHeaders().getContentType().getType()).isEqualTo("image");
