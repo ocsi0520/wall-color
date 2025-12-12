d3a47d6773dd12ad007a342e6cc73b50a3f20115
Faragó Tamás <ocsi0520@gmail.com>
2025-12-12 14:51:48 +0100

    remove unused NoContextTestApp

--- color-manager/src/test/java/com/my_wall_color/test_utils/NoContextTestApp.java
+++
@@ -1,8 +0,0 @@
-package com.my_wall_color.test_utils;
-
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-
-@SpringBootConfiguration
-@EnableAutoConfiguration
-public class NoContextTestApp {}
