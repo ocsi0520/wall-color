3d43b23e0d1f13d2dad7e2952fc2714cb525bc56
Faragó Tamás <ocsi0520@gmail.com>
2025-12-15 23:08:39 +0100

    remove boilerplate from JpaColor with lombok

--- color-manager/src/main/java/com/my_wall_color/color_manager/color/jpa/JpaColor.java
+++ color-manager/src/main/java/com/my_wall_color/color_manager/color/jpa/JpaColor.java
@@ -1,9 +1,15 @@
 package com.my_wall_color.color_manager.color.jpa;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
 @Entity
 @Table(name = "Color")
+@Data
+@NoArgsConstructor
 public class JpaColor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,63 +21,4 @@ public class JpaColor {
 
     private String name;
     private Integer recorded_by;
-
-    public JpaColor(Integer id, short red, short green, short blue, String name, Integer recorded_by) {
-        this.id = id;
-        this.red = red;
-        this.green = green;
-        this.blue = blue;
-        this.name = name;
-        this.recorded_by = recorded_by;
-    }
-
-    public JpaColor() {}
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public short getRed() {
-        return red;
-    }
-
-    public void setRed(short red) {
-        this.red = red;
-    }
-
-    public short getGreen() {
-        return green;
-    }
-
-    public void setGreen(short green) {
-        this.green = green;
-    }
-
-    public short getBlue() {
-        return blue;
-    }
-
-    public void setBlue(short blue) {
-        this.blue = blue;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getRecorded_by() {
-        return recorded_by;
-    }
-
-    public void setRecorded_by(Integer recorded_by) {
-        this.recorded_by = recorded_by;
-    }
 }
