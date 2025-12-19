ca0a8b7cdadf775caeaab3b428d3e4225e8cac30
Faragó Tamás <ocsi0520@gmail.com>
2025-12-19 11:16:38 +0100

    add ROLE_ADMIN to UserDetailsMapper

--- color-manager/src/main/java/com/my_wall_color/color_manager/security/UserDetailsMapper.java
+++ color-manager/src/main/java/com/my_wall_color/color_manager/security/UserDetailsMapper.java
@@ -3,6 +3,7 @@ package com.my_wall_color.color_manager.security;
 import com.my_wall_color.color_manager.user.domain.User;
 import org.mapstruct.Mapper;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
@@ -14,7 +15,10 @@ public class UserDetailsMapper {
         return new UserDetails() {
             @Override
             public Collection<? extends GrantedAuthority> getAuthorities() {
-                return List.of();
+                if (user.getIsAdmin())
+                    return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"));
+                else
+                    return List.of();
             }
 
             @Override
