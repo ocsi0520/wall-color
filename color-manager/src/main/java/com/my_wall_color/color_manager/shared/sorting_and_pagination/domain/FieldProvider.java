c30f69f35655b6162746d7c255455ee3994d9346
Faragó Tamás <ocsi0520@gmail.com>
2025-12-19 17:58:08 +0100

    add domain SortAndPagination with FieldProvider
    goal is to create a spring-independent SortAndPagination class with strongly-typed fields

---
+++ color-manager/src/main/java/com/my_wall_color/color_manager/shared/sorting_and_pagination/domain/FieldProvider.java
@@ -0,0 +1,8 @@
+package com.my_wall_color.color_manager.shared.sorting_and_pagination.domain;
+
+import java.io.Serializable;
+import java.lang.constant.Constable;
+
+public interface FieldProvider extends Constable, Serializable {
+    String getFieldName();
+}
