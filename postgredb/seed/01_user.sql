528d343d9a4ad2d8248ddcadf4a1b70c0de8410c
Faragó Tamás <ocsi0520@gmail.com>
2025-12-19 10:23:04 +0100

    update seed data for postgresql container with is_admin flag

--- postgredb/seed/01_user.sql
+++ postgredb/seed/01_user.sql
@@ -3,26 +3,30 @@ CREATE TABLE App_User(
     username VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(60) NOT NULL,
     -- https://stackoverflow.com/a/5882472
-    name VARCHAR(70) NOT NULL -- https://stackoverflow.com/a/30509
+    name VARCHAR(70) NOT NULL, -- https://stackoverflow.com/a/30509
+    is_admin boolean NOT NULL
 );
-INSERT INTO App_User(username, password, name)
+INSERT INTO App_User(username, password, name, is_admin)
 VALUES (
         'jdoe',
         -- pw: user1
         '$2a$12$/wnuwqCoou1NwfDGzAPTFOsDgbyIblbOyGp.8WRvPMYt/GWSn8XYy',
-        'John Doe'
+        'John Doe',
+        TRUE
     );
-INSERT INTO App_User(username, password, name)
+INSERT INTO App_User(username, password, name, is_admin)
 VALUES (
         'donna',
         -- pw: user2
         '$2a$12$OhrUMXgjSnPXpGkiKkzwSeYkXPDI6IqhpM6/h6blyKE6lDhPPjLNa',
-        'Donna Da Maria'
+        'Donna Da Maria',
+        TRUE
     );
-INSERT INTO App_User(username, password, name)
+INSERT INTO App_User(username, password, name, is_admin)
 VALUES (
         'Alex',
         -- pw: user3
         '$2a$12$y1ZToAjRAP.yor.J5lY8/uHwB2w1wFJ/CxhbU1V.9cyRezcC2sS2i',
-        'Alex Scrowatz'
+        'Alex Scrowatz',
+        FALSE
     );
