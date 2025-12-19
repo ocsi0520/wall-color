8e7d814ef628c7a402e3902c3607a6378791b7b2
Faragó Tamás <ocsi0520@gmail.com>
2025-12-19 17:58:29 +0100

    add ColorField

---
+++ color-manager/src/main/java/com/my_wall_color/color_manager/color/domain/ColorField.java
@@ -0,0 +1,15 @@
+package com.my_wall_color.color_manager.color.domain;
+
+import com.my_wall_color.color_manager.shared.sorting_and_pagination.domain.FieldProvider;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public enum ColorField implements FieldProvider {
+    NAME("name"), DEFAULT("id");
+    private String fieldName;
+
+    @Override
+    public String getFieldName() {
+        return this.fieldName;
+    }
+}
