3d5aa3bafc4ea820a253cff3b54e18224c1254c4
Faragó Tamás <ocsi0520@gmail.com>
2025-12-21 18:07:26 +0100

    allow sorting for multiple columns in SortAndPagination

--- color-manager/src/main/java/com/my_wall_color/color_manager/shared/sorting_and_pagination/domain/SortAndPagination.java
+++ color-manager/src/main/java/com/my_wall_color/color_manager/shared/sorting_and_pagination/domain/SortAndPagination.java
@@ -3,11 +3,12 @@ package com.my_wall_color.color_manager.shared.sorting_and_pagination.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.LinkedHashMap;
+
 @Data
 @AllArgsConstructor
 public class SortAndPagination<AFieldProvider extends FieldProvider> {
     private int pageSize;
     private int pageIndex;
-    private AFieldProvider fieldProvider;
-    private boolean isAscendant;
+    private LinkedHashMap<AFieldProvider, Boolean> sorting;
 }
