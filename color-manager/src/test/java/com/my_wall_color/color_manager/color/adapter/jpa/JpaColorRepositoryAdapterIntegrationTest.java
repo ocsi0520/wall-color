6af5a7b3eb9127ed75fc5a04f3ad787f7a3ff201
Faragó Tamás <ocsi0520@gmail.com>
2025-12-23 22:31:48 +0100

    add findAll to ColorRepository with SortAndPagination

--- color-manager/src/test/java/com/my_wall_color/color_manager/color/adapter/jpa/JpaColorRepositoryAdapterIntegrationTest.java
+++ color-manager/src/test/java/com/my_wall_color/color_manager/color/adapter/jpa/JpaColorRepositoryAdapterIntegrationTest.java
@@ -3,12 +3,16 @@ package com.my_wall_color.color_manager.color.adapter.jpa;
 
 import com.my_wall_color.color_manager.color.domain.Color;
 import com.my_wall_color.color_manager.IntegrationTest;
+import com.my_wall_color.color_manager.color.domain.ColorField;
+import com.my_wall_color.color_manager.shared.sorting_and_pagination.domain.SortAndPagination;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -42,5 +46,42 @@ class JpaColorRepositoryAdapterIntegrationTest extends IntegrationTest {
     void shouldReturn3ColorsForJdoeUser() {
         List<Color> colorsForJdoeUser = unitUnderTest.findAllAssociatedWith(userFixture.jdoe.getId());
         assertThat(colorsForJdoeUser).isEqualTo(List.of(colorFixture.sulyom, colorFixture.havasiGyopar));
+    }
+
+    @Test
+    void shouldReturnThirdAndFourthColor() {
+        var actual = unitUnderTest.findAll(new SortAndPagination<>(2, 1, new LinkedHashMap<>()));
+        assertThat(actual).containsExactly(
+                colorFixture.kekSzelloRozsa,
+                colorFixture.szarkalab
+        );
+    }
+
+    @Test
+    void shouldReturnNothingDueToExceedingPageSize() {
+        var actual = unitUnderTest.findAll(new SortAndPagination<>(5, 5, new LinkedHashMap<>()));
+        assertThat(actual).isEmpty();
+    }
+
+    @Test
+    void shouldReturnFourthAndThirdColor() {
+        var sort = new LinkedHashMap<>(Map.of(ColorField.NAME, false));
+        var actual = unitUnderTest.findAll(new SortAndPagination<>(2, 1, sort));
+        assertThat(actual).containsExactly(colorFixture.palastfu, colorFixture.kekSzelloRozsa);
+    }
+
+    @Test
+    void shouldReturnAllColorByReverse() {
+        var sort = new LinkedHashMap<>(Map.of(ColorField.ID, false));
+        var actual = unitUnderTest.findAll(new SortAndPagination<>(10, 0, sort));
+        assertThat(actual).containsExactly(
+                colorFixture.palastfu,
+                colorFixture.havasiEukaliptusz,
+                colorFixture.havasiGyopar,
+                colorFixture.szarkalab,
+                colorFixture.kekSzelloRozsa,
+                colorFixture.brazilMenta,
+                colorFixture.sulyom
+        );
     }
 }
