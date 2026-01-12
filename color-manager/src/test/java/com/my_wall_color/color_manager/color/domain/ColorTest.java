a69c796fa4cdb82cd81f27dccc1cfea5cefb131f
Faragó Tamás <ocsi0520@gmail.com>
2026-01-12 11:55:54 +0100

    make ColorTest test-cases parameterized

--- color-manager/src/test/java/com/my_wall_color/color_manager/color/domain/ColorTest.java
+++ color-manager/src/test/java/com/my_wall_color/color_manager/color/domain/ColorTest.java
@@ -1,23 +1,44 @@
 package com.my_wall_color.color_manager.color.domain;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ColorTest {
-    @Test
-    void shouldNotAcceptInvalidColorComponent() {
-        assertThrows(IllegalArgumentException.class, () -> Color.create(null, 255, 255, Short.MIN_VALUE, "min blue", 1));
-        assertThrows(IllegalArgumentException.class, () -> Color.create(null, 255, 256, 0, "green above 1", 1));
-        assertThrows(IllegalArgumentException.class, () -> Color.create(null, Short.MAX_VALUE, 255, 0, "max red", 1));
-        assertThrows(IllegalArgumentException.class, () -> Color.create(null, -1, 255, 0, "red below 1", 1));
+    @ParameterizedTest
+    @MethodSource("invalidColorComponents")
+    void shouldNotAcceptInvalidColorComponent(
+            int red, int green, int blue, String name) {
+
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> Color.create(null, red, green, blue, name, 1)
+        );
     }
 
-    @Test
-    void shouldNotAcceptEmptyName() {
-        assertThrows(IllegalArgumentException.class, () -> Color.create(null, 255, 255, 0, "", 1));
-        assertThrows(IllegalArgumentException.class, () -> Color.create(null, 255, 255, 0, null, 1));
+    static Stream<Arguments> invalidColorComponents() {
+        return Stream.of(
+                Arguments.of(255, 255, Short.MIN_VALUE, "min blue"),
+                Arguments.of(255, 256, 0, "green above 255"),
+                Arguments.of(Short.MAX_VALUE, 255, 0, "max red"),
+                Arguments.of(-1, 255, 0, "red below 0")
+        );
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    void shouldNotAcceptEmptyName(String name) {
+        assertThrows(
+                IllegalArgumentException.class,
+                () -> Color.create(null, 255, 255, 0, name, 1)
+        );
     }
 
     @Test
