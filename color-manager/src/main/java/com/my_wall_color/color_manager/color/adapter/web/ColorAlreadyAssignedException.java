431c5525638e2463b7061d1db3f93078345727e5
Faragó Tamás <ocsi0520@gmail.com>
2026-01-12 12:23:12 +0100

    make ColorAlreadyAssignedException checked

--- color-manager/src/main/java/com/my_wall_color/color_manager/color/adapter/web/ColorAlreadyAssignedException.java
+++ color-manager/src/main/java/com/my_wall_color/color_manager/color/adapter/web/ColorAlreadyAssignedException.java
@@ -1,6 +1,6 @@
 package com.my_wall_color.color_manager.color.adapter.web;
 
-public class ColorAlreadyAssignedException extends RuntimeException {
+public class ColorAlreadyAssignedException extends Exception {
     public ColorAlreadyAssignedException(int colorId, String username) {
         super("Color " + colorId + " is already assigned to user " + username);
     }
