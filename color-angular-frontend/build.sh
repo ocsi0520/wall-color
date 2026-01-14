64ac090862f320c30fa43e247a9eca1fe5ff4cb0
Faragó Tamás <ocsi0520@gmail.com>
2026-01-14 14:49:54 +0100

    unify frontend build.sh with others

--- color-angular-frontend/build.sh
+++ color-angular-frontend/build.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+set -e
+
 GREEN_COLOR='\033[0;32m'
 NO_COLOR='\033[0m'
 
@@ -5,18 +8,19 @@ PORT=4200
 FRONTEND_CONTAINER_NAME="wall-color-frontend"
 FRONTEND_IMAGE_NAME="${FRONTEND_CONTAINER_NAME}-image"
 
-podman stop $FRONTEND_CONTAINER_NAME;
-podman rm $FRONTEND_CONTAINER_NAME;
-echo -e "${GREEN_COLOR}removed ${FRONTEND_CONTAINER_NAME} container${NO_COLOR}"
+podman stop $FRONTEND_CONTAINER_NAME 2>/dev/null || true
+podman rm $FRONTEND_CONTAINER_NAME 2>/dev/null || true
+podman image rm $FRONTEND_IMAGE_NAME 2>/dev/null || true
 
-podman image rm $FRONTEND_IMAGE_NAME
-echo "removed ${FRONTEND_IMAGE_NAME} image"
-podman build -f deployment/Containerfile -t $FRONTEND_IMAGE_NAME . && \
+echo -e "${GREEN_COLOR}Removed previous container and image${NO_COLOR}"
+
+podman build -f deployment/Containerfile -t $FRONTEND_IMAGE_NAME .
 echo -e "${GREEN_COLOR}re-built ${FRONTEND_IMAGE_NAME} image${NO_COLOR}"
 
 podman run \
+    -d --name $FRONTEND_CONTAINER_NAME \
+    --network wall-color-net \
     -e NGINX_PORT=${PORT} \
     -p ${PORT}:${PORT} \
-    -d --name $FRONTEND_CONTAINER_NAME \
     $FRONTEND_IMAGE_NAME && \
 echo -e "${GREEN_COLOR}$FRONTEND_CONTAINER_NAME container is running${NO_COLOR}"
