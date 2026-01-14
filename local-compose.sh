68e3e389792f21604f3d33fe50172012163074c3
Faragó Tamás <ocsi0520@gmail.com>
2026-01-14 19:26:03 +0100

    add local-compose script

---
+++ local-compose.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+CMD="podman-compose \
+  -f project-compose.local.yaml \
+  -f ./postgredb/deployment/podman-compose.local.yaml \
+  -f ./color-manager/deployment/podman-compose.local.yaml \
+  -f ./color-angular-frontend/deployment/podman-compose.local.yaml"
+
+case "$1" in
+  "up")
+    $CMD up
+    ;;
+  "up-build")
+    $CMD up --build
+    ;;
+  "down")
+    $CMD down
+    ;;
+  *)
+    echo "Usage: $0 {up|up-build|down}"
+    ;;
+esac
