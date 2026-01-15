605ad3b57b9b36f3c4608db21f475d2a02bb4087
Faragó Tamás <ocsi0520@gmail.com>
2026-01-15 10:52:30 +0100

    extend local-compose with docker

--- local-compose.sh
+++ local-compose.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
-CMD="podman-compose \
+if command -v podman >/dev/null 2>&1; then
+    COMPOSE_CMD="podman compose"
+elif command -v docker >/dev/null 2>&1; then
+    COMPOSE_CMD="docker compose"
+else
+    echo "Error: neither podman nor docker is installed." >&2
+    exit 1
+fi
+
+CMD_WITH_YAMLS="${COMPOSE_CMD} \
   -f project-compose.local.yaml \
   -f ./postgresdb/deployment/podman-compose.local.yaml \
   -f ./color-manager/deployment/podman-compose.local.yaml \
@@ -8,13 +17,13 @@ CMD="podman-compose \
 
 case "$1" in
   "up")
-    $CMD up
+    $CMD_WITH_YAMLS up
     ;;
   "up-build")
-    $CMD up --build
+    $CMD_WITH_YAMLS up --build
     ;;
   "down")
-    $CMD down
+    $CMD_WITH_YAMLS down
     ;;
   *)
     echo "Usage: $0 {up|up-build|down}"
