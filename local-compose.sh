1e98cbec5c57cbca1f900bc1d4c5a2777be3ac59
Faragó Tamás <ocsi0520@gmail.com>
2026-01-16 18:12:28 +0100

    move reverse-proxy to its own folder

--- local-compose.sh
+++ local-compose.sh
@@ -11,6 +11,7 @@ fi
 
 CMD_WITH_YAMLS="${COMPOSE_CMD} \
   -f project-compose.local.yaml \
+  -f ./reverse-proxy/deployment/podman-compose.local.yaml \
   -f ./postgresdb/deployment/podman-compose.local.yaml \
   -f ./color-manager/deployment/podman-compose.local.yaml \
   -f ./color-angular-frontend/deployment/podman-compose.local.yaml"
