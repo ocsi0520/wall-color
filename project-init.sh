3fd6cc17fc6111eb9317b96ce7a666da5935db6c
Faragó Tamás <ocsi0520@gmail.com>
2026-01-17 15:20:47 +0100

    extract env-init & jwt-cert-init from project-init

--- project-init.sh
+++ project-init.sh
@@ -25,31 +25,8 @@ ask_allow_vibe_code() {
     done
 }
 
-if ! [ -f ".env" ] ; then
-    tail env-example.txt -n +4 > .env
-    echo ".env is created from env-example.txt"
-else
-    echo ".env exists already"    
-fi
-
-RESOURCES_DIR="./color-manager/src/main/resources/certs"
-PRIVATE_KEY="$RESOURCES_DIR/private.pem"
-PUBLIC_KEY="$RESOURCES_DIR/public.pem"
-
-if ! [ -d "$RESOURCES_DIR" ]; then
-    mkdir $RESOURCES_DIR;
-    echo "created $RESOURCES_DIR directory"
-fi
-
-if ! [ -f "$PRIVATE_KEY" ] || ! [ -f "$PUBLIC_KEY" ]; then
-    echo "public-private pairs are not present, about to create"
-    openssl genrsa 4096 > $PRIVATE_KEY
-    openssl rsa -in $PRIVATE_KEY -pubout -out $PUBLIC_KEY
-    echo "public-private pairs are created in $RESOURCES_DIR"
-else
-    echo "public-private keys already exist"
-fi
-
+sh "./project-init/env-init.sh"
+sh "./project-init/jwt-cert-init.sh"
 
 REV_PROXY_DEP_DIR="./reverse-proxy/deployment/certs"
 
