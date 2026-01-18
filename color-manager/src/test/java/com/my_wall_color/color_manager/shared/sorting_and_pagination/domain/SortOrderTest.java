1db1fc4f5f3ea5a62c156fb15d1aafe1748f7dbb
Faragó Tamás <ocsi0520@gmail.com>
2026-01-18 15:11:06 +0100

    remove not working exclusion of jacoco + one more test for SortOrder
    created separate ticket for exclusion

--- color-manager/src/test/java/com/my_wall_color/color_manager/shared/sorting_and_pagination/domain/SortOrderTest.java
+++ color-manager/src/test/java/com/my_wall_color/color_manager/shared/sorting_and_pagination/domain/SortOrderTest.java
@@ -3,6 +3,7 @@ package com.my_wall_color.color_manager.shared.sorting_and_pagination.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.my_wall_color.color_manager.color.domain.ColorField;
+import java.util.Date;
 import org.junit.jupiter.api.Test;
 
 class SortOrderTest {
@@ -31,5 +32,12 @@ class SortOrderTest {
     var order1 = new SortOrder<>(TestTranslationEnum.ONE, SortOrder.Direction.ASCENDING);
     var order2 = new SortOrder<>(ColorField.ID, SortOrder.Direction.ASCENDING);
     assertThat(order1).isNotEqualTo(order2);
+  }
+
+  @Test
+  void shouldSayDifferentForEntirelyDifferentType() {
+    var order1 = new SortOrder<>(TestTranslationEnum.ONE, SortOrder.Direction.ASCENDING);
+    Date d = new Date();
+    assertThat(order1).isNotEqualTo(d);
   }
 }
